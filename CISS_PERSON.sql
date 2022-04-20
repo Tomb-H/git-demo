f4c2d137a4114f01823299f5f05f25d12875171e
Hui <350084287@qq.com>
2022-04-21 01:37:51 +0800

    yxkj@wyh

---
+++ CISS_PERSON.sql
@@ -0,0 +1,5 @@
+insert into CISS.PERSON (PERSON_ID, PNAME, AGE) values (1, '张三', 24);
+insert into CISS.PERSON (PERSON_ID, PNAME, AGE) values (4, '张三', 24);
+insert into CISS.PERSON (PERSON_ID, PNAME, AGE) values (5, '张三', 24);
+insert into CISS.PERSON (PERSON_ID, PNAME, AGE) values (6, '李四', 24);
+insert into CISS.PERSON (PERSON_ID, PNAME, AGE) values (7, '李四', 40);
